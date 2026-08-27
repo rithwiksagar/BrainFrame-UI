36961c35b5ca5591c06ae507fadfe5edb5b1e979
rithwiksagar <rithwiksagar6@gmail.com>
2026-08-27 17:00:03 +0530

    refactor: enhance topbar layout and theme toggle UI

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import UseToggleTheme from "@/hooks/UseToggleTheme";
-import { Github, PanelLeft } from "lucide-react";
+import { Github, GithubIcon, PanelLeft, Star } from "lucide-react";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import { Dispatch, RefObject, SetStateAction } from "react";
@@ -13,19 +13,25 @@ export const TopBar = ({ topbarRef, setOpen }: topBarProps) => {
   return (
     <div
       ref={topbarRef}
-      className="fixed left-0 top-0 flex w-full z-50 px-5 md:px-15 py-3 justify-between items-center border-b border-neutral-200 dark:border-neutral-800 backdrop-blur-xs"
+      className="fixed left-0 top-0 flex w-full z-50 px-5 md:px-8 py-3 justify-between items-center border-b border-neutral-200 dark:border-neutral-800 backdrop-blur-xs"
     >
       <div className="">
         <Link
           href={"/"}
-          className="text-xl md:text-2xl font-medium tracking-wide"
+          className="text-md md:text-lg  font-mono tracking-wide"
         >
           BrainFrame.
         </Link>
       </div>
-      <div className="flex gap-6">
-        <Link href="https://github.com/rithwiksagar/Zero2">
-          <Github className="size-4.5" />
+      <div className="flex gap-14 pr-6">
+        <Link
+          className="border rounded-sm flex items-center gap-2 px-2 py-1 border-neutral-300 dark:border-neutral-800 text-neutral-700 dark:text-neutral-300 font-semibold text-[13px]"
+          href="https://github.com/rithwiksagar/Zero2"
+        >
+          <Github className="size-4" />
+          <span className="border-l border-neutral-300 pl-2 dark:border-neutral-800">
+            GitHub
+          </span>
         </Link>
         <div className="flex justify-center items-center">
           <UseToggleTheme />
--- hooks/UseToggleTheme.tsx
+++ hooks/UseToggleTheme.tsx
@@ -18,9 +18,15 @@ export default function UseToggleTheme() {
       className="rounded-xl cursor-pointer absolute"
     >
       {resolvedTheme === "dark" ? (
-        <Moon className="size-4.5 text-white/90" />
+        <div className="border rounded-sm flex items-center gap-2 px-2 py-1 border-neutral-300 dark:border-neutral-800 text-neutral-700 dark:text-neutral-300 font-semibold text-[13px]">
+          <Moon className="size-4 text-white/90" />
+          <span className="border-l border-neutral-300 pl-2 dark:border-neutral-800">Dark</span>
+        </div>
       ) : (
+        <div className="border rounded-sm flex items-center gap-2 px-2 py-1 border-neutral-300 dark:border-neutral-800 text-neutral-700 dark:text-neutral-300 font-semibold text-[13px]">
         <Sun className="size-4.5 bg text-neutral-900" />
+        <span className="border-l border-neutral-300 pl-2 dark:border-neutral-800">Dark</span>
+        </div>
       )}
     </button>
   );
