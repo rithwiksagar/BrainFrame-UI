cc7e9295d3970f34144027310a65eef52f1047db
rithwiksagar <rithwiksagar6@gmail.com>
2026-08-28 11:45:09 +0530

    refactor: update Home page layout, adjust spacing, remove unused image section

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const Tools = [
     icon: <FaReact />,
   },
   {
-    title: "Tailwind CSS",
+    title: "Tailwind",
     icon: <RiTailwindCssFill />,
   },
   {
@@ -28,13 +28,13 @@ export default function Home() {
     <main className="min-h-screen w-full bg-foreground text-background relative">
       <Navbar />
 
-      <section className="px-28 pt-40">
+      <section className="px-4 md:px-28 pt-28 md:pt-40">
         <div className="max-w-[560px]">
-          <h1 className="text-5xl font-medium font-serif leading-[1.05] tracking-tight max-w-xl">
+          <h1 className="text-3xl md:text-5xl font-medium font-serif leading-[1.05] tracking-tight max-w-md md:max-w-xl">
             Build AI products that don&apos;t look generic
           </h1>
 
-          <p className="mt-4 max-w-xl text-lg leading-7 text-neutral-500">
+          <p className="mt-4 max-w-xl text-[15px] md:text-lg leading-7 text-neutral-500">
             Premium, production-ready AI components with polished interactions
             and zero styling headaches.
           </p>
@@ -47,7 +47,7 @@ export default function Home() {
                 "shadow-[0_2px_4px_rgba(0,0,0,0.06),0_8px_20px_rgba(0,0,0,0.08)]",
                 "hover:bg-neutral-100",
                 "active:bg-neutral-900 active:shadow-[0_1px_2px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.2)]",
-                "rounded-full px-4 py-2",
+                "rounded-full px-4 py-2 select-none",
               )}
             >
               Get Started
@@ -59,7 +59,7 @@ export default function Home() {
                 "shadow-[0_2px_4px_rgba(0,0,0,0.2),0_8px_20px_rgba(0,0,0,0.25)]",
                 "hover:bg-neutral-700",
                 "active:bg-neutral-900 active:shadow-[0_1px_2px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.2)]",
-                "rounded-full px-4 py-2",
+                "rounded-full px-4 py-2 select-none",
               )}
             >
               Components
@@ -67,24 +67,17 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="absolute right-0 top-0 h-screen w-120 rounded-lg bg-white p-2.5">
-          <div className="relative h-full w-full overflow-hidden rounded-lg shadow-xl">
-            <Image
-              src="/image.png"
-              alt="Brainframe UI"
-              fill
-              className="object-cover translate-y-24 scale-125"
-              priority
-            />
-          </div>
-        </div>
+        <Chatbot />
 
-        <div className="mt-54">
-          <div className="flex items-center gap-8">
+        <div className="mt-20 md:mt-54">
+          <h6 className="md:hidden flex justify-center mb-6 text-sm text-neutral-400">
+            Built With
+          </h6>
+          <div className="grid grid-cols-2 md:grid-cols-8 gap-4 justify-items-center md:justify-items-start">
             {Tools.map((t) => (
               <div
                 key={t.title}
-                className="flex items-center gap-2 text-xl text-neutral-500 hover:text-neutral-800"
+                className="flex items-center md:gap-1 gap-2 text-md md:text-xl text-neutral-500 hover:text-neutral-800"
               >
                 <span>{t.icon}</span>
                 {t.title}
@@ -99,12 +92,15 @@ export default function Home() {
 
 function Navbar() {
   return (
-    <nav className="w-5xl flex items-center justify-between px-28 py-7 font-mono">
-      <Link href="/" className="text-xl font-semibold font-mono tracking-tight">
+    <nav className="w-screen md:w-5xl flex items-center justify-between px-4 md:px-28 py-7 font-mono">
+      <Link
+        href="/"
+        className="text-md md:text-xl font-semibold font-mono tracking-tight"
+      >
         Brainframe UI
       </Link>
 
-      <div className="flex items-center gap-8 text-sm">
+      <div className="flex items-center gap-4 md:gap-8 text-sm">
         <Link
           href="/docs/introduction"
           className="text-neutral-600 hover:text-black"
@@ -119,11 +115,23 @@ function Navbar() {
         >
           Github
         </Link>
-
-        <Link href="#sponser" className="text-neutral-600 hover:text-black">
-          Sponser
-        </Link>
       </div>
     </nav>
+  );
+}
+
+function Chatbot() {
+  return (
+    <div className="mt-20 md:mt-auto md:absolute md:right-0 md:top-0 h-120 md:h-screen md:w-120 rounded-lg bg-white p-1 md:p-2.5 select-none">
+      <div className="relative h-full w-full overflow-hidden rounded-lg shadow-xl">
+        <Image
+          src="/image.png"
+          alt="Brainframe UI"
+          fill
+          className="object-cover md:translate-y-24 scale-125"
+          priority
+        />
+      </div>
+    </div>
   );
 }
