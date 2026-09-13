ac5305caf845d555900886142f20c094c3e9ee2d
rithwiksagar <rithwiksagar6@gmail.com>
2026-09-13 11:02:19 +0530

    feat: add MosaicAskAi component and playground page

---
+++ app/playground/004/page.tsx
@@ -0,0 +1,9 @@
+import { MosaicAskAi } from "@/registry/new-york/MosaicAskAI/MosaicAskAI";
+
+
+
+export default function Play(){
+    return <div className="flex min-h-screen items-center justify-center">
+        <MosaicAskAi />
+    </div>
+}
